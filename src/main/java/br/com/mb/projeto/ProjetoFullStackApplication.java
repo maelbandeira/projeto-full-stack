47290a86158a46c59eb432ee5606cbdd52dbb2b5
Abmael Santana <maelbandeira@gmail.com>
2020-12-04 21:32:01 -0300

    projeto iniciado

---
+++ src/main/java/br/com/mb/projeto/ProjetoFullStackApplication.java
@@ -0,0 +1,13 @@
+package br.com.mb.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mb/projeto/ProjetoFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mb.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
